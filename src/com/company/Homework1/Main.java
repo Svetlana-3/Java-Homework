0e8882c57fe8a466ebccc8fde58be65e4f3165d6
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-02-21 14:16:20 +0200

    First commit

---
+++ src/com/company/Homework1/Main.java
@@ -0,0 +1,19 @@
+package com.company.Homework1;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        double far;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet temperatūru Fārenheita grādos: ");
+        far = scanner.nextDouble();
+        scanner.close();
+        System.out.println("Ļoti interesanti, paldies!");
+        double cen;
+        cen = (5 * (far - 32))/9;
+        System.out.println("Temperatūra Celsija grādos būs: " + cen);
+        System.out.println("Lai Tev laba diena!");
+    }
+}
