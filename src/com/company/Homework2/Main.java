5dd5711b89c6128ab275991f233bfa9b07a49ae8
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-02-26 15:16:28 +0200

    Java-Homework2 done

---
+++ src/com/company/Homework2/Main.java
@@ -0,0 +1,21 @@
+package com.company.Homework2;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet veselo skaitli X no -10 līdz 10: ");
+        int x = scanner.nextInt();
+        System.out.println("Ievadiet veselo skaitli Y no -10 līdz 10: ");
+        int y = scanner.nextInt();
+        scanner.close();
+
+        if ((x >= -2 && x <= 3 && y <= 1 && y >= -4) && !((x == -2 || x == 3) && (y == 1 || y == -4))) {
+            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas figurās iekšpusē!");
+        }
+        else if ((x == -2 || x == 3) && (y == 1 || y == -4)) {
+            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas uz figūras līnijas!");
+        } else {
+            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas ārpus figūras!");
+        }
+    }
+}
