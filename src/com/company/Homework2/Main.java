d6078c23c01ebc13cde016fb1456271e636dd535
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-03-01 16:46:58 +0200

    Java-Homework2 ar labojumu done

--- src/com/company/Homework2/Main.java
+++ src/com/company/Homework2/Main.java
@@ -9,13 +9,12 @@ public class Main {
         int y = scanner.nextInt();
         scanner.close();
 
-        if ((x >= -2 && x <= 3 && y <= 1 && y >= -4) && !((x == -2 || x == 3) && (y == 1 || y == -4))) {
+        if (x > -2 && x < 3 && y < 1 && y > -4) {
             System.out.println("Koordinātas (" + x + "; " + y + ") atrodas figurās iekšpusē!");
         }
-        else if ((x == -2 || x == 3) && (y == 1 || y == -4)) {
+        else if ((x >= -2 && x <= 3) && (y <= 1 && y >= -4)) {
             System.out.println("Koordinātas (" + x + "; " + y + ") atrodas uz figūras līnijas!");
-        } else {
+        } else
             System.out.println("Koordinātas (" + x + "; " + y + ") atrodas ārpus figūras!");
-        }
     }
 }
