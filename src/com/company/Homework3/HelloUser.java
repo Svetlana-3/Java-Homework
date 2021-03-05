c97205d817764f8b67a60cc243e203592382cc72
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-03-05 09:50:08 +0200

    Java Homework3 done

--- src/com/company/Homework3/HelloUser.java
+++ src/com/company/Homework3/HelloUser.java
@@ -8,13 +8,12 @@ public class HelloUser {
         System.out.println("Ievadiet vārdu un uzvārdu vienā rindā ar atstarpi: ");
         String text1 = scanner.next();
         String text2 = scanner.nextLine();
-
         scanner.close();
 
-        String output1 = text1.substring(0, 1).toUpperCase() + text1.substring(1);
+        String output1 = text1.substring(0, 1).toUpperCase() + text1.substring(1).toLowerCase();
         String output2 = text2.substring(text2.lastIndexOf(" ") + 1).toUpperCase();
 
         System.out.println(output1);
-        System.out.println("\'"+ output2+"\'");
+        System.out.println("\'" + output2 + "\'");
     }
 }
