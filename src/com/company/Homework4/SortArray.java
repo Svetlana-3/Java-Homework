a1e1df44d556e8599b9a8c96c943d2b37125f351
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-03-10 14:44:02 +0200

    Java-Homework4 done

--- src/com/company/Homework4/SortArray.java
+++ src/com/company/Homework4/SortArray.java
@@ -5,21 +5,24 @@ import java.util.Scanner;
 
 public class SortArray {
     public static void main(String[] args) {
-        int cip, arr[];
-        System.out.println("Ievadi masīvu ar izmēru no 20 līdz 30: ");
-        Scanner scanner = new Scanner(System.in);
-        cip = scanner.nextInt();
-        arr = new int[cip];
-        scanner.close();
+        int cip;
+        int arr[];
+        System.out.println("Ievadi masīvu ar izmēru no 20 līdz 40: ");
+            Scanner scanner = new Scanner(System.in);
+            cip = scanner.nextInt();
+            if (cip < 20 || cip > 40) {
+                System.out.println("Diemžēl nepareizi! Izlasi vēlreiz uzdevumu.");
+                return;
+            }
+            arr = new int[cip];
+            scanner.close();
 
-        Random rnd = new Random();
-        int randomInt = rnd.nextInt(11) + 90; // 2) Masīvs tiek aizpildīts ar gadījuma skaitļiem(int) robežās no 10 līdz 99
-        for (int i = 0; i < arr.length; i++)
-            arr[i] = (int) (Math.random() * randomInt); // Kā bez Math.random????
-        //arr [i] = randomInt;
-        for (int i : arr) {
-            System.out.print(i + ", "); // 3) Izvadam uz ekrāna visus masīvā esošos skaitļus atdalītus ar komatu un atstarpi
+        for (int i = 0; i < arr.length; i++) {
+            Random rnd = new Random();
+            arr[i] = rnd.nextInt(89) + 11;
+            System.out.print(arr[i] + ", ");
         }
+
         // otrā daļa
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++)
@@ -32,5 +35,6 @@ public class SortArray {
         for (int i = 0; i < arr.length; ++i) {
             System.out.println(arr[i]); // 5) Izvadam uz ekrāna sakārtotos masīva elementus
         }
+
     }
 }
